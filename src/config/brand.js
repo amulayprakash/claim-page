975c9cf5459a5b32a5cc824b4c62bb2c2a273da4
Yash Gupta <guptayash200010@gmail.com>
2026-08-06 09:23:56 +0530

    updates

--- src/config/brand.js
+++ src/config/brand.js
@@ -12,7 +12,7 @@ export const BRAND_NAME = isFusdt ? 'FUSDT Claim' : 'USBT Claim'
 export const BRAND_DOMAIN = isFusdt ? 'claim.fusdt.online' : 'claim.usbt.online'
 export const BRAND_DESCRIPTION = isFusdt ? `${BRAND_SYMBOL} Token Promotional Giveaway Claim` : 'USBT Token Promotional Giveaway Claim'
 export const BRAND_COMPANY = isFusdt ? 'FUSDT Operations, S.A. de C.V.' : 'USBT Operations, S.A. de C.V.'
-export const BRAND_LOGO = isFusdt ? '/tokens/fusdt-lolo.png' : '/tokens/usbt-lolo.png'
+export const BRAND_LOGO = isFusdt ? '/tokens/fusdt-lolo.svg' : '/tokens/usbt-lolo.png'
 export const BRAND_HERO_IMAGE = isFusdt ? '/fusdt_hero.png' : '/usbt_hero.png'
 
 export function getBrandConfig() {
